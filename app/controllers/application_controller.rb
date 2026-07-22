22e02ffbc99c24f2dd4ea5dde9148a8d50ddce74
AlejandroEspinolaDev <alejandroespinola37@gmaill.com>
2026-07-22 15:40:43 -0300

    chore: add authentication method in ApplicationController & create method for POST action in UsersController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+    # execute 'authenticate_user' before any request
+    before_action :authenticate_user
+
+    def authenticate_user
+        # read HTTP header
+        header = request.headers['Authorization']
+
+        # grab token. E.g: 'Bearer token123' gets -> 'token123'. '&' use for avoid crash if value is nil
+        token = header&.split(' ')&.last
+
+        # decodes token. If correct, returns payload (E.g: { user_id: 1, username: ... })
+        payload = JsonWebToken.decode(token)
+
+        # find user in DB by id if payload is exists
+        @current_user = User.find_by(id: payload[:user_id]) if payload
+
+        # return HTTP 401 if @current_user = nil
+        render json: { error: 'Unauthorized' }, status :unauthorized unless @current_user
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,37 @@
 class UsersController < ApplicationController
+  # avoid authenticate_user while create request, because user still don't have a token
+  skip_before_action :authenticate_user, only: [ :create ]
 
   def create
+    # create a new user. user_params makes sure that info is valid
+    @user = User.new(user_params)
+
+    # if user correctly save in DB
+    if @user.save
+      # encode payload (user_id) with JWT
+      token = JsonWebToken.encode(user_id: @user.id)
+
+      # return json with data. 'UserSerializer' use for avoid sending password_digest in JSON response
+      render json: @user, serializer: UserSerializer, meta: { token: token }, status: :created
+    else
+      # return errors
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
   end
   
+  private
+  def user_params
+    # establish what params are allowed when creating a new user
+    params.require(:user).permit(:username, :password)
+  end
 end
